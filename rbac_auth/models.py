189c8fe9be478e4857fc78969820586015c96c35
ragapriya-goml <ragapriya.k@goml.io>
2025-06-27 16:22:09 +0530

    BASIC RBAC AUTHORIZATION

--- rbac_auth/models.py
+++ rbac_auth/models.py
@@ -8,7 +8,6 @@ from rest_framework.permissions import BasePermission
 class Role(models.Model):
     name = models.CharField(max_length=64, unique=True)
     description = models.TextField(blank=True)
-
     def __str__(self):
         return self.name
 
